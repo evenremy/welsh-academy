289979e9fc6d3e494d12b2d95f9305fe75a61325
rev <remy.even@interieur.gouv.fr>
2022-11-03 14:47:40 +0100

    auto migration

--- api/go.mod
+++ api/go.mod
@@ -2,7 +2,11 @@ module api
 
 go 1.19
 
-require github.com/zeromicro/go-zero v1.4.2
+require (
+	github.com/golang-migrate/migrate/v4 v4.15.2
+	github.com/lib/pq v1.10.7
+	github.com/zeromicro/go-zero v1.4.2
+)
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -18,8 +22,9 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/lib/pq v1.10.7 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
@@ -40,6 +45,7 @@ require (
 	go.opentelemetry.io/otel/sdk v1.11.0 // indirect
 	go.opentelemetry.io/otel/trace v1.11.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/automaxprocs v1.5.1 // indirect
 	golang.org/x/net v0.0.0-20220531201128-c960675eff93 // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
--- api/internal/svc/servicecontext.go
+++ api/internal/svc/servicecontext.go
@@ -3,7 +3,14 @@ package svc
 import (
 	"api/internal/config"
 	"api/model/ingredient"
+	"fmt"
 	"github.com/zeromicro/go-zero/core/stores/postgres"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+
+	"github.com/golang-migrate/migrate/v4"
+	postgresmigrate "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 type ServiceContext struct {
@@ -14,10 +21,37 @@ type ServiceContext struct {
 func NewServiceContext(c config.Config) *ServiceContext {
 
 	conn := postgres.New(c.Postgre.Datasource)
-	// TODO : init db schema
+
+	// DB automatic migration
+	if err := initDB(conn); err != nil {
+		fmt.Println(err.Error())
+	}
 
 	return &ServiceContext{
 		Config:          c,
 		IngredientModel: ingredient.NewIngredientsModel(conn),
 	}
+}
+
+// Return an error if the migration fails.
+// Do not alter the db if the schema is up-to-date.
+// Based on the schema_migration table versus content of the sql folder.
+func initDB(conn sqlx.SqlConn) error {
+	db, err := conn.RawDB()
+	if err != nil {
+		return err
+	}
+	driver, err := postgresmigrate.WithInstance(db, &postgresmigrate.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://sql",
+		"postgres", driver)
+	if err != nil {
+		return err
+	}
+
+	if err = m.Up(); err != nil {
+		return err
+	}
+
+	return nil
 }
