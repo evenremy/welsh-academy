6422d65f1d2d175fc9d9b2aaabb0c437b9c72c8e
Remy EVEN <evenremy@gmail.com>
2022-11-06 17:25:40 +0100

    Added AddRecipe implementation

--- api/internal/logic/addrecipelogic.go
+++ api/internal/logic/addrecipelogic.go
@@ -1,7 +1,11 @@
 package logic
 
 import (
+	quantity2 "api/model/quantity"
+	"api/model/recipe"
+	stage2 "api/model/stage"
 	"context"
+	"database/sql"
 
 	"api/internal/svc"
 	"api/internal/types"
@@ -24,7 +28,77 @@ func NewAddRecipeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddReci
 }
 
 func (l *AddRecipeLogic) AddRecipe(req *types.AddRecipeReq) (resp *types.AddRecipeReply, err error) {
-	// todo: add your logic here and delete this line
+	// Recipe
+	newRecipe := recipe.Recipes{
+		Title: req.Title,
+		Owner: sql.NullInt64{},
+	}
+	err = newRecipe.Description.Scan(req.Description)
+	if err != nil {
+		return nil, err
+	}
+	recipeId, err := l.svcCtx.RecipeModel.InsertReturningId(l.ctx, &newRecipe)
+	if err != nil {
+		l.Logger.Errorf("Error during insertion for Recipe : id = %d", recipeId)
+		return nil, err
+	}
 
-	return
+	err = l.addIngredients(req.IngredientList, recipeId)
+	if err != nil {
+		return nil, err
+	}
+
+	err = l.addStages(req.StageList, recipeId)
+	if err != nil {
+		return nil, err
+	}
+
+	reply := types.AddRecipeReply{
+		Id:    recipeId,
+		Title: req.Title,
+	}
+
+	return &reply, nil
+}
+
+func (l *AddRecipeLogic) addIngredients(ingredients []types.LinkIngredientWithQuantity, recipeId int64) error {
+	for i, ingredient := range ingredients {
+		newQuantity := quantity2.Quantity{
+			Recipe:     recipeId,
+			Ingredient: ingredient.IngredientId,
+			Unit:       ingredient.Unit,
+			Quantity:   sql.NullFloat64{},
+		}
+		err := newQuantity.Quantity.Scan(ingredient.Quantity)
+		if err != nil {
+			return err
+		}
+		_, err = l.svcCtx.QuantityModel.Insert(l.ctx, &newQuantity)
+		if err != nil {
+			l.Logger.Errorf("Error during insertion in Quantity at %d/%d ingredients, for: %+v", i, len(ingredients), newQuantity)
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *AddRecipeLogic) addStages(stages []types.Stage, recipeId int64) error {
+	for i, stage := range stages {
+		newStage := stage2.Stages{
+			Recipe:      recipeId,
+			Order:       int64(stage.Order),
+			Description: sql.NullString{},
+		}
+		err := newStage.Description.Scan(stage.Description)
+		if err != nil {
+			return err
+		}
+
+		_, err = l.svcCtx.StageModel.Insert(l.ctx, &newStage)
+		if err != nil {
+			l.Logger.Errorf("Error during insertion in Stages at %d/%d stages, for: %+v", i, len(stages), newStage)
+			return err
+		}
+	}
+	return nil
 }
--- api/model/recipe/recipesmodel.go
+++ api/model/recipe/recipesmodel.go
@@ -1,6 +1,10 @@
 package recipe
 
-import "github.com/zeromicro/go-zero/core/stores/sqlx"
+import (
+	"context"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 var _ RecipesModel = (*customRecipesModel)(nil)
 
@@ -9,12 +13,24 @@ type (
 	// and implement the added methods in customRecipesModel.
 	RecipesModel interface {
 		recipesModel
+		InsertReturningId(ctx context.Context, data *Recipes) (int64, error)
 	}
 
 	customRecipesModel struct {
 		*defaultRecipesModel
 	}
+
+	returningId struct {
+		Id int64 `db:"id"`
+	}
 )
+
+func (c customRecipesModel) InsertReturningId(ctx context.Context, data *Recipes) (int64, error) {
+	var query = fmt.Sprintf("insert into %s (%s) values ($1, $2, $3) returning id", c.table, recipesRowsExpectAutoSet)
+	resp := returningId{}
+	err := c.conn.QueryRowCtx(ctx, &resp, query, data.Title, data.Description, data.Owner)
+	return resp.Id, err
+}
 
 // NewRecipesModel returns a model for the database table.
 func NewRecipesModel(conn sqlx.SqlConn) RecipesModel {
