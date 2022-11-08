20736f7d0df9d7cb128bd5a11fb45a619870e1ea
rev <remy.even@interieur.gouv.fr>
2022-11-08 15:59:01 +0100

    not working tests : handler not formatting error to json

---
+++ api/internal/handler/addfavoriterecipehandler_test.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"api/internal/types"
+	"context"
+	"github.com/maxatome/go-testdeep/helpers/tdhttp"
+	"github.com/maxatome/go-testdeep/td"
+	"net/http"
+	"testing"
+)
+
+func TestAddFavoriteRecipeHandlerErrors(t *testing.T) {
+	testApi := tdhttp.NewTestAPI(t, AddFavoriteRecipeHandler(testCtx))
+	body := &types.FavReq{
+		UserId:   999999,
+		RecipeId: 99999,
+	}
+	testApi.PostJSON("/favorite_recipe", body).
+		CmpStatus(http.StatusForbidden).
+		CmpJSONBody(`
+{
+	"msg": NotEmpty()
+	"code": Gt(0)
+}
+`)
+}
+
+func TestAddFavoriteRecipeHandler(t *testing.T) {
+	userId, err := testCtx.UserModel.InsertReturningId(context.Background(), "abc", false)
+	if err != nil {
+		return
+	}
+	testApi := tdhttp.NewTestAPI(t, AddFavoriteRecipeHandler(testCtx))
+	body := &types.FavReq{
+		UserId:   userId,
+		RecipeId: 999999,
+	}
+	testApi.PostJSON("/favorite_recipe", body).
+		CmpStatus(http.StatusForbidden).
+		CmpJSONBody(td.JSON(`
+{
+	"msg": NotEmpty()
+	"code": Gt(0)
+}
+`))
+}
